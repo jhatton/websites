913779c742a132f00cf74e3d0e6a2e4d60a2870b
Jim Hatton <jhatton@fullsail.edu>
2013-12-03 00:34:41 -0600

    moved code around

--- index.php
+++ index.php
@@ -5,6 +5,23 @@
 <title>myBlog</title>
 </head>
 
-<body>
+ <body>
+ <div id="main">  
+    <h1>My Simple Blog</h1>  
+    <div id="blogPosts">  
+  <?php  
+	  	include 'blogsql.php';  
+	  	$blogPosts = GetBlogPosts();
+		foreach ($blogPosts as $post)  
+{  
+    echo "<div class='post'>";  
+    echo "<h1>" . $post->title . "</h1>";  
+    echo "<p>" . $post->post . "</p>";  
+    echo "<span class='footer'>Posted By: " . $post->author . " Posted On: " . $post->datePosted . " Tags: " . $post->tags . "</span>";  
+    echo "</div>";  
+}  	  	  
+?>     
+ </div>  
+</div>  
 </body>
 </html>
