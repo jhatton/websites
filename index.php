5eb3d3357220604bb1321dcf8534f164caac8ccd
Jim Hatton <jhatton@fullsail.edu>
2013-12-06 21:40:53 -0600

    got code working now

--- index.php
+++ index.php
@@ -11,16 +11,25 @@
  <div id="main">  
    <h1>Welcome to myBlog</h1>
 <?php
-/* Connect to an ODBC database using an alias */
+/* Connect to an  database  */
 $dsn = 'mysql:host=127.0.0.1;dbname=myblog;port=8889';
 $user = 'root';
 $password = 'root';
 
 try {
     $dbh = new PDO( $dsn, $user, $password);
+	 
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
+
+$statement = $dbh->prepare("
+	SELECT first_name, last_name, email
+	FROM people
+	WHERE (id>0)
+	ORDER BY first_name, last_name, email
+");
+var_dump($statement);
 
 ?>    
     <footer>
