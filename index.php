d7590d7419e828a1b13d1e25b8adff432e7f3081
Jim Hatton <jhatton@fullsail.edu>
2013-12-06 20:49:23 -0600

    code not working
    I did as the video said and won’t connect to database

--- index.php
+++ index.php
@@ -2,34 +2,27 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<link rel="stylesheet" href="css/style.css">
-
-<title>myBlog</title>
+<link href="stylesheet" href="css/style.css">
+ <title>myBlog</title>
 </head>
 
  <body>
  <!--main title-->
  <div id="main">  
    <h1>Welcome to myBlog</h1>
-<?php  
-	$dsn="mysql:host=127.0.0.01;port=8889;dbname=myblog";
-	$db_user="root";
-	$db_pass="root";
-	$db=new PDO($dsn, $db_user, $db_pass);
-	$statement= $db->prepare("
-		SELECT first_name, last_name, email
-		FROM person 
-		WHERE (id>0)
-			AND (first_name IS NOT NULL);
-			AND (last_name IS NOT NULL);
-			AND (email IS NOT NULL);
-		ORDER BY first_name, last_name, email
-	");
-	if ($statement->execute()){
-		$rows=$statement>fetchAll(\PDO::FETCH_ASSOC);
-		}
-?>     
-    
+<?php
+/* Connect to an ODBC database using an alias */
+$dsn = 'myblog';
+$user = 'root';
+$password = 'root';
+
+try {
+    $dbh = new PDO(mysql:host=127.0.0.1; $dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+?>    
     <footer>
     <p>&copy; 2013 Jim Hatton || Full Sail University || BDF1312</p> 
     </footer>
