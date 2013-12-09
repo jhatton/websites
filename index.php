911fd225a2e7ae32cedeef3b0b39eafdddfdf9e9
Jim Hatton <jhatton@fullsail.edu>
2013-12-09 01:00:45 -0600

    changing login check

--- index.php
+++ index.php
@@ -8,14 +8,16 @@ $model= new db(DSN, USER, PASS);
 $view=new authView();
 
 $username=empty($_POST['username']) ? '' : strtolower(trim($_POST['username']));
-$username=empty($_POST['password']) ? '' : trim($_POST['username']);
-
+$username=empty($_POST['password']) ? '' : trim($_POST['password']);
+$contentPage="form";
 if (!empty($username) && !empty($password)){
 	$user = $model->getUserByNamePass($username, $password);
+	$contentPage='success';
 }
 
 $rows=$model->getDataBase();
 $view=new pageView;
+$view->show($contentPage);
 $view->showHeader('myBlog');
 $view->showFooter();
 
