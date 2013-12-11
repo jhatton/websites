3cff2d30541c21051c28e3543ebfc24a02654509
Jim Hatton <jhatton@fullsail.edu>
2013-12-10 20:54:23 -0600

    inserting comments on pages

--- views/authView.php
+++ views/authView.php
@@ -3,9 +3,9 @@
 class AuthView{
 	
 	public function show($template, $data=array()) {
-		$template = "view/${template}.inc";
+		$template = "views/${template}.inc";
 		if (file_exists($templatePath)){
-			include $template;
+			include $templatePath;
 		}	
 		
 	}
