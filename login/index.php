2f903fd155e44d49af14f0cf964fc1533be16eb6
Jim Hatton <jhatton@fullsail.edu>
2013-12-15 00:39:28 -0600

    added views for modelView

--- login/index.php
+++ login/index.php
@@ -3,9 +3,10 @@
 include "controllers/dsn.php";
 include "views/header.inc";
 include "views/footer.inc";
+include "models/modelView.php";
 
 $model=new dataBase(DSN, USER, PASS);
-
+$views=new modelView();
 $username=empty($_POST['username']) ? '' : strtolower(trim($_POST['username']));
 $username=empty($_POST['password']) ? '' : trim($_POST['password']);
 
