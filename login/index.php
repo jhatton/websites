40086bf8d30b43687d2fff2ff53ec8a4c6836366
Jim Hatton <jhatton@fullsail.edu>
2013-12-15 00:19:43 -0600

    login page

--- login/index.php
+++ login/index.php
@@ -4,4 +4,12 @@ include "controllers/dsn.php";
 include "views/header.inc";
 include "views/footer.inc";
 
- 
+ $model=new dataBase(DSN, USER, PASS);
+
+$username=empty($_POST['username']) ? '' : strtolower(trim($_POST['username']));
+$username=empty($_POST['password']) ? '' : trim($_POST['password']);
+
+if (!empty($username) && !empty($password)){
+	$user = $model->getUserByNamePass($username, $password);
+	$views->getView("views/login.inc");
+}
