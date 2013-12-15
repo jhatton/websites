09209d9b4edc3b0c3eb86ba211f7e03387ce92a4
Jim Hatton <jhatton@fullsail.edu>
2013-12-15 01:08:44 -0600

    deleted code from index

--- login/index.php
+++ login/index.php
@@ -7,10 +7,3 @@ include "models/modelView.php";
 
 $model=new dataBase(DSN, USER, PASS);
 $views=new modelView();
-$username=empty($_POST['username']) ? '' : strtolower(trim($_POST['username']));
-$username=empty($_POST['password']) ? '' : trim($_POST['password']);
-
-if (!empty($username) && !empty($password)){
-	$user = $model->getUserByNamePass($username, $password);
-	$views->getPages("views/login.inc");
-}
